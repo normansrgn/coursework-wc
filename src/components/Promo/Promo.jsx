9bd23073b64b54bd5ee81ef9021891ba7d60b9fa
norma <normanmagic2004@gmail.com>
2024-10-09 14:14:12 +0300

    promo section

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-
 import "./promo.scss";
 
 export default function Promo() {
   return (
     <>
       <div className="promo">
-        <h2 className="promo__title">Иследуй виртуальную реальность</h2>
-        <p>
-          Виртуальная реальностью может быть веселой, но это также может быть
-          очень страшным. Проведи свое время незабываемо в VR-клубе
-        </p>
+        <h2 className="promo__title">Исследуй виртуальную реальность</h2>
+        <div>
+          Виртуальная реальность может быть веселой, но это также может быть
+          очень страшной. Проведи свое время незабываемо в VR-клубе.
+        </div>
         <div className="promo__buttons">
-            <button className="promo__btnFirst">Выбрать игру</button>
-            <button className="promo__btnSecond">Наши цены</button>
+          <button className="promo__btnFirst">Выбрать игру</button>
+          <button className="promo__btnSecond">Наши цены</button>
         </div>
       </div>
     </>
