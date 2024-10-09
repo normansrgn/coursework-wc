3bc62fd1362023300ae5a221814b1047b16e31ed
norma <normanmagic2004@gmail.com>
2024-10-09 12:09:18 +0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,11 @@ import React from "react";
 import "./Header.scss";
 
 export default function Header() {
-    return (
-        <div className="header">
-            <div className="header__logo">WReality</div>
-        </div>
-    );
+  return (
+    <div className="header">
+      <div className="header__logo">
+        W<span>R</span>eality
+      </div>
+    </div>
+  );
 }
