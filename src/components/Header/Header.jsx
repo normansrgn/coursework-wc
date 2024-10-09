e608a197317747eb7cda3b750d3a8fbfb3600b72
norma <normanmagic2004@gmail.com>
2024-10-09 13:32:47 +0300

    $

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,6 +8,20 @@ export default function Header() {
       <div className="header__logo">
         W<span>R</span>eality
       </div>
+
+      <div className="header__contacts">
+        <ol className="header__contactList">
+          <li className="header__contactItem">О клубе</li>
+          <li className="header__contactItem">Залы</li>
+          <li className="header__contactItem">Игры</li>
+          <li className="header__contactItem">Контакты</li>
+        </ol>
+
+        <div className="header__Bron">
+          <button>Забронировать</button>
+          <div className="header__bronNumb">8 (925)880-15-12</div>
+        </div>
+      </div>
     </div>
   );
 }
